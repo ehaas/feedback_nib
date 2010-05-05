808d3604f652ded83340c52699822d1be11b2b29
Evan Haas <evanhaas@gmail.com>
2010-05-05 14:29:24 -0700

    fix tempaltes

--- generators/feedback_nib/feedback_nib_generator.rb
+++ generators/feedback_nib/feedback_nib_generator.rb
@@ -7,7 +7,7 @@ class FeedbackNibGenerator < Rails::Generator::Base
 
       m.directory 'app/views/feedback'
       m.file 'views/feedback/_feedback_nib.html.haml', 'app/views/feedback/_feedback_nib.html.haml'
-      m.file 'views/feedback/index.html.haml', 'app/views/feedback/_feedback_nib.html.haml'
+      m.file 'views/feedback/index.html.haml', 'app/views/feedback/index.html.haml'
       
       m.file 'javascripts/feedback.js', 'public/javascripts/feedback.js'
       m.file 'stylesheets/sass/feedback_nib.sass', 'public/stylesheets/sass/feedback_nib.sass'
